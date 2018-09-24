f85762d69523f33b535e0d22afe283d0df83a77d
= <matita@lapis.com>
2018-09-25 00:53:37 +0200

    fix

--- js/terminal.js
+++ js/terminal.js
@@ -1,25 +1,19 @@
-//  JavaScript is a lot like a shopping centre in a recession
-//  There's a lot of closures
 (function(doc, win) {
 
-    //  If you don't have QSA, you must hate me
     if(!doc.querySelector) {
         return alert('Your browser sucks. Sorry.');
     }
 
-    //  I never node her that well, Officer.
     var list = doc.querySelector('ul'),
         all = list.children,
         i = all.length,
         html = [];
 
-    //  Censorship, yo
     while(i--) {
         html[i] = all[i].innerHTML;
         all[i].innerHTML = '';
     }
 
-    //  Go all Frankenstein and shit
     var animate = function(i) {
         var me = all[i],
 
@@ -29,16 +23,11 @@
             //  D = line delay
             c = 0, d = 0;
 
-        //  Censor the page
         me.className = add;
         list.style.marginTop = '-' + (((i + 1) * 25) / 2) + 'px';
 
         var inty = setInterval(function() {
-            //  MOAR TEXTS
             me.innerHTML = html[i].substr(0, c) + '<span class="typing">|</span>';
-
-            //  What's the best programming language in the world?
-            //  Not this one.
             c++;
 
             if(html[i].length < c) {
@@ -56,16 +45,29 @@
 
             if(i== all.length)
             {
-                var options = {
-                    "animate": true,
-                    "patternWidth": 500,
-                    "patternHeight": 500,
-                    "grainOpacity": 1,
-                    "grainDensity": 1,
-                    "grainWidth": 1,
-                    "grainHeight": 1
+                // setInterval(function(){
+                    var options = {
+                        "animate": true,
+                        "patternWidth": 500,
+                        "patternHeight": 500,
+                        "grainOpacity": 1,
+                        "grainDensity": 1,
+                        "grainWidth": 1,
+                        "grainHeight": 1
+                    }
+                    grained("#container", options);
+                // }, 100);
+
+                list = doc.querySelector('ul');
+                all = list.children;
+                i = all.length;
+                html = [];
+
+                while(i--) {
+                    html[i] = all[i].innerHTML;
+                    all[i].innerHTML = '';
                 }
-                grained("#container", options);
+                $( "<p>404</p>" ).insertAfter( "ul" );                
             }
 
         }, 75);
